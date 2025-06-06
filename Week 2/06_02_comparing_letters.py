bdd2b40c3dbb4f08ea4d9a12eb9f15e06ca3a966
nithilgithub <AA.SC.P2MCA2307733@ahead.students.amrita.edu>
2025-06-06 17:48:12 +0530

    commit

---
+++ Week 2/06_02_comparing_letters.py
@@ -0,0 +1,18 @@
+# can you figure out the logic here?
+
+ay = "a"
+bee = "b"
+
+# which one is bigger?
+
+print(ay > bee)
+
+# why??
+
+# The comparison between characters in Python is based on their Unicode code points.
+
+# here is a hint: check out the ord() function
+# How does python store string characters under the hood?
+
+# look up what ord() does online and report back!
+# The ord() function in Python takes a string of a single Unicode character as an argument and returns an integer representing its Unicode code point.
