52ef4c8b3b581ca7068e89c198a12841041c350a
nithilgithub <AA.SC.P2MCA2307733@ahead.students.amrita.edu>
2025-06-06 17:32:30 +0530

    commit

---
+++ Week 2/06_01_comparisons_on_ints.py
@@ -0,0 +1,25 @@
+# Run some basic comparisons on basic integers and floating points
+
+# what is bigger, a or b?
+a = 2
+b = 10
+
+print("Is a bigger than b?", a > b)
+# What is smaller , c or d?
+c = 2.02
+d = 2.01119999
+
+print("Is c smaller than d?", c < d)
+
+# what is bigger e or f?
+e = float("inf")
+f = 12912912912091928312903713582043754302895723048957
+
+print("Is e bigger than f?", e > f)
+
+# are these equal?
+
+g = 1.02020202020
+i = 1.0202020202011111
+
+print("Is g equal to i?", g == i)
