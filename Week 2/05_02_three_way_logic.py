5bfde86d13c552aa38ace793e947a63bb78e93f7
nithilgithub <AA.SC.P2MCA2307733@ahead.students.amrita.edu>
2025-06-06 11:54:34 +0530

    commit

--- Week 2/05_02_three_way_logic.py
+++ Week 2/05_02_three_way_logic.py
@@ -7,15 +7,26 @@ c = True
 
 # Print the value of:
 # what do you think: print(a and b or c)  will return? Why?
+print(a and b or c)
 # Does the order of operations matter?
 # is print(a or b and c) different?
+print(a or b and c)
 
 # Assign c to false and print the value of a and b or c
 a = True
 b = False
 c = False
 
+print(a and b or c)
+
 # Understand the difference in each scenario
 # what is happening here?
 
 # now try to use some ()'s to force python to evaluate it differently.
+# For example:
+a = True
+b = False
+c = False
+
+print((a and b) or c)   # Parentheses force 'a and b' to be evaluated first
+print(a and (b or c))   # Parentheses force 'b or c' to be evaluated first
